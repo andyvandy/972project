a91611008b6d966f001ffa8c995a249fff337cb4
Andrew van den Hoeven <andrew.vandenhoeven@scotiabank.com>
2017-10-29 12:56:22 -0400

    added scaffolding, downloading data

---
+++ code/download_data.py
@@ -0,0 +1,26 @@
+'''
+script to download data from yahoo finance
+'''
+
+import os
+import pandas as pd
+import pandas_datareader.data as web # need to install seperatel from pandas
+import datetime
+from tqdm import *
+
+def main():
+    start = datetime.datetime(2010, 1, 1)
+    end = datetime.datetime(2014, 12, 31)
+    companies_df=pd.DataFrame.from_csv('../data/companylist.csv')
+    for ticker in tqdm(companies_df.index):
+        try:
+            save_stock_close_data(ticker,start,end)
+        except:
+            pass
+
+def save_stock_close_data(ticker,start,end):
+    f = web.DataReader(ticker, 'yahoo', start, end)[['Adj Close']] # only want adjusted close
+    f.to_csv(os.path.join("../data","stocks",ticker+".csv"))
+
+if __name__ =="__main__":
+    main()
---
+++ code/main.py
@@ -0,0 +1,21 @@
+'''
+Main file to run all of our code from. Not much to say yet
+
+'''
+
+import argparse
+from algorithms import online_newton
+from algorithms import offline_newton # the base against which we compute regret
+from algorithms import constant_rebalance
+
+import numpy as np
+import pandas as pd
+
+def main():
+    pass
+
+
+
+
+if __name__ =="__main__":
+    main()
